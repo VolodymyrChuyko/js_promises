6dedf90bfba13fe9f24b0569c521309d8ae7e43d
Volodymyr Chuiko <volodymyr.chuiko@sigma.software>
2023-02-02 11:15:50 +0200

    Update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
-//Initial Configuration
-// module.exports = {
-//   env: {
-//     browser: true,
-//     es2021: true,
-//     node: true,
-//   },
-//   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-//   parser: '@typescript-eslint/parser',
-//   plugins: ['@typescript-eslint'],
-//   root: true,
-// };
-
-// Linting with Type Information
 module.exports = {
   env: {
     browser: true,
@@ -32,5 +18,24 @@ module.exports = {
   ],
   rules: {
     'semi': 'error',
-  }
+    'arrow-parens': ["error", "always"],
+    'no-multi-spaces': "error",
+    'no-multiple-empty-lines': ["error", { "max": 1, "maxEOF": 0 }],
+    'no-trailing-spaces': "error",
+    'eol-last': ["error", "always"],
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/restrict-template-expressions': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-floating-promises': 'off',
+      },
+    },
+  ]
 };
